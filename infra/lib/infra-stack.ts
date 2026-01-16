076c84fa50838b30b29de32f00f255acff0b5e92
Ennio Haibach <ennio.haibach@gmail.com>
2026-01-17 00:06:04 +0100

    refactor: [SW-19] Clean up after merge

--- infra/lib/infra-stack.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-
-export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-  }
-}
